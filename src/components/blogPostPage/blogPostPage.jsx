7e53f4ef464970d573255bfd314575d76d0e28d5
Sam Henderson <sam@192-168-1-108.tpgi.com.au>
2023-05-30 20:59:13 +1000

    Created dynamic blog post pages

--- src/components/blogPostPage/blogPostPage.jsx
+++ src/components/blogPostPage/blogPostPage.jsx
@@ -1,6 +1,24 @@
+import { useParams } from "react-router-dom"
+import blogPosts from '../../blogPosts/blogPosts.json'
+
 const BlogPostPage = () => {
+
+    const {slug} = useParams();
+
+    console.log(slug)
+
     return(
         <div>
+            {blogPosts.map((posts)=>{
+                if(posts.slug == slug){
+                    return( 
+                        <div>
+                    <h1>{posts.blogPostTitle}</h1>
+                    <p>{posts.blogPostBodyCopy}</p>
+                    </div>
+                    )
+                }
+            })}
             
         </div>
     )
