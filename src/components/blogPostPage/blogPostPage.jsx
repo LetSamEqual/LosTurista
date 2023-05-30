068bbba999890452394d785c756789153aa0fdfa
Sam Henderson <sam@192-168-1-108.tpgi.com.au>
2023-05-30 21:07:36 +1000

    fixed return error but still needs more fixing

--- src/components/blogPostPage/blogPostPage.jsx
+++ src/components/blogPostPage/blogPostPage.jsx
@@ -1,27 +1,30 @@
-import { useParams } from "react-router-dom"
-import blogPosts from '../../blogPosts/blogPosts.json'
+import { useParams } from "react-router-dom";
+import blogPosts from "../../blogPosts/blogPosts.json";
 
 const BlogPostPage = () => {
+  const { slug } = useParams();
 
-    const {slug} = useParams();
+  console.log(slug);
 
-    console.log(slug)
+  return (
+    <div>
+      {blogPosts.map((posts) => {
+        if (posts.slug === slug) {
+          return (
+            <div>
+              <h1>{posts.blogPostTitle}</h1>
+              <p>{posts.blogPostBodyCopy}</p>
+            </div>
+          );
+        }
+        else {return (
+          <div>
+            <h1>404</h1>
+          </div>
+        )};
+      })}
+    </div>
+  );
+};
 
-    return(
-        <div>
-            {blogPosts.map((posts)=>{
-                if(posts.slug == slug){
-                    return( 
-                        <div>
-                    <h1>{posts.blogPostTitle}</h1>
-                    <p>{posts.blogPostBodyCopy}</p>
-                    </div>
-                    )
-                }
-            })}
-            
-        </div>
-    )
-}
-
-export default BlogPostPage
+export default BlogPostPage;
