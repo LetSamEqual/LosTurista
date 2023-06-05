17575b9a6934f9e9eec1cf1c4e5ff758a5ad8688
Sam Henderson <sam@192-168-1-108.tpgi.com.au>
2023-06-05 16:30:11 +1000

    tiles now grow on hover

--- src/components/blogPostTiles/blogPostTiles.jsx
+++ src/components/blogPostTiles/blogPostTiles.jsx
@@ -1,17 +1,20 @@
 import "./blogPostTiles.css";
 
-
 const BlogPostTile = ({ blogPostData }) => {
-
   const backgroundImageStyle = {
     backgroundImage: `url("${blogPostData.tileImageBackground}")`,
-    backgroundSize: 'cover',
-  }
-
+    backgroundSize: "cover",
+  };
 
   return (
     <div className="tileContainer">
-      <div className="tileImage" style={backgroundImageStyle}></div>
+      <div className="tileImage" style={backgroundImageStyle}>
+        <img
+          src={blogPostData.tileImageBackground}
+          alt={blogPostData.tileImageBackgroundAlt}
+          className="tileThumbnail"
+        />
+      </div>
       <div className="tileTextContainer">
         <div className="tileHeadingContainer">
           <h2 className="tileHeading">{blogPostData.tileTitle}</h2>
