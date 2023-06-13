d6538dfdeba6e019787f165784843d86a7115125
Sam Henderson <sam@192-168-1-108.tpgi.com.au>
2023-06-13 15:59:07 +1000

    fixed styling for main page on devices

--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -13,22 +13,29 @@ const LandingPage = () => {
       </div>
       <div className="headingImageContainer">
         <div className="blogIntroContainer">
-          <h2 className="blogIntroTitle">Dear sir/madam</h2>
-          <h3 className="blogIntroSubheader">
-            Thank you so much for your enquiry. Unfortunately I’m unable to help
-            with this matter as I’m currently out of office, on the other side
-            of the world, attempting to avoid emails just like yours.
-          </h3>
-          <h3 className="blogIntroSubheader">
-            Luckily the reception here is patchy, and my return date is
-            impossible to say, so I don’t expect to reply soon or ever really.
-          </h3>
-          <h3 className="blogIntroSubheader">
-            If you need assistance in the meantime, I suggest trying Google, or
-            else turning it off then back on again.
-          </h3>
-          <h3 className="blogIntroSubheader">Warm regards,</h3>
-          <h3 className="blogIntroSubheader">Sam Henderson</h3>
+          <h2 className="blogIntroTitle">
+            To: All <br />
+            Subject: Out of office
+          </h2>
+          <div className="blogIntroSubheaderContainer">
+            <h3 className="blogIntroSubheader">Dear sir/madam</h3>
+            <h3 className="blogIntroSubheader">
+              Thank you so much for your enquiry. Unfortunately I’m
+              unable to help with this matter as I’m currently out of office, on
+              the other side of the world, attempting to avoid emails just
+              like yours.
+            </h3>
+            <h3 className="blogIntroSubheader">
+              Luckily the reception here is patchy, and my return date is
+              impossible to say, so I don’t expect to reply soon or ever really.
+            </h3>
+            <h3 className="blogIntroSubheader">
+              If you need assistance in the meantime, I suggest trying Google,
+              or else turning it off then back on again.
+            </h3>
+            <h3 className="blogIntroSubheader">Warm regards,</h3>
+            <h3 className="blogIntroSubheader">Sam Henderson</h3>
+          </div>
         </div>
       </div>
       <div className="blogPostTilesContainer">
